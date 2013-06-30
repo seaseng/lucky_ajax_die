9d7745972274e1abe9664a85d17bd79a61459556
Navid Mostafavi <navidm@gmail.com>
2013-06-29 22:58:23 -0700

    completed the ajax and send back roll object

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,9 +7,23 @@ end
 post '/rolls' do
   # If the user passes-in a "value", let's use it. Otherwise, we'll generate a random one.
   # See: roll_if_value_is_nil method in the Roll model.
+
+  #if user didn't send number we can create one
   value = params[:value] ? params[:value].to_i : nil
+  roll = value ? Roll.create({ value: value }) : Roll.create
 
-  @roll = value ? Roll.create({ value: value }) : Roll.create
+  if request.xhr?
+    # in this case JS already sent number through URL
+    puts "Params: #{params}"
+    content_type :json
+    # @roll_value = params[:value]
+    roll.to_json # send it back as JSON
+    # I could also just send back roll.value
 
-  erb :index  # HINT: what does this do? what should we do instead?
+  else
+    # set instance var, if nil then create one    
+    
+    erb :index  
+  end
+
 end
--- public/js/application.js
+++ public/js/application.js
@@ -7,4 +7,32 @@ $(document).ready(function () {
   //   4- use jQuery to submit an AJAX post to the form's action
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
 
+  console.log('update');
+  var generateRandomNum = function (min, max) {
+    return Math.floor(Math.random()*(max - min + 1)) + min;
+  };
+
+  $('form').on('submit', function(evnt) {
+    evnt.preventDefault();
+    var roll_value = generateRandomNum(1, 6);
+    // debugger;
+
+    //need to be in JS object form, key: value, for params hash
+    var die_roll = { value: roll_value };
+
+    var ajax_submit = $.post('/rolls', die_roll);
+
+    ajax_submit.done(function( response ) {
+
+      console.log('AJAX response: ' + response.roll.value);
+      var value = response.roll.value; //ruby objects attr still holds
+      var filename = value + '.png';
+      
+      htmlString = '<img src=/' + filename + ' title=' + value + ' alt="the roll">';
+      $('#die').html(htmlString);
+
+    });
+
+  });
+
 });
